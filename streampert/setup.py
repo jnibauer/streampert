3b43459fe3d0d0019a9c964337af8cce00469f2d
jnibauer <jnibauer@princeton.edu>
2025-05-12 09:36:38 -0400

    initial commit

---
+++ streampert/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='streampert',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    description='A short description of your package.',
+    python_requires='>=3.7',
+)
+
